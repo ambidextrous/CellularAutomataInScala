66c87d5429964a95c371519e7e8a12b2e99896db
Aidan <butleraidan@gmail.com>
2017-11-14 21:27:17 +0100

    Actor naming error resolved

--- cellularAutomata.scala
+++ cellularAutomata.scala
@@ -1,7 +1,10 @@
 import scala.util.Random
-import akka.actor.Actor
-import akka.actor.ActorSystem
-import akka.actor.Props
+import akka.actor._
+import akka.pattern.ask
+import akka.util.Timeout
+import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.concurrent.duration._
+import scala.language.postfixOps
 
 class Creature(maxLifespanc: Int = 0, fitnessc: Double = 0, namec: String = "-") {
     val maxLifespan: Int = getMaxLifespan(maxLifespanc)
@@ -27,56 +30,46 @@ class Creature(maxLifespanc: Int = 0, fitnessc: Double = 0, namec: String = "-")
     }
 }
 
-class NodeManager extends Actor {
-    // Simulation state isolated in this variable, each
+class NodeManager() extends Actor {
     var resident: Creature = new Creature()
 
     def receive = {
-        case attacker: Creature => attackResident(attacker) 
-        case _ => println("Unknown command")
-    }
-
-    def attackResident(attacker: Creature) = {
-        val r = scala.util.Random
-        if (r.nextFloat <= (attacker.fitness - resident.fitness)) {
-            resident = new Creature(attacker.maxLifespan, attacker.fitness, attacker.name)
-        }
+        case "requestName" =>
+            sender ! resident.name
+            //val r = scala.util.Random
+            //if (r.nextFloat <= (attacker.fitness - resident.fitness)) {
+            //    resident = new Creature(attacker.maxLifespan, attacker.fitness, attacker.name)
+            //}
     }
 }
 
-//val world = Array.ofDim[Creature](10,10) 
-
-object cellularAutomata {
+object cellularAutomata  {
     def main(args: Array[String]) {
         val world = generateWorld()
-        //world.foreach(populate)
-        //println(world(0)(0))
         printWorld(world)
     }
 
-    def generateWorld() : Array[Array[Creature]] = {
+    def generateWorld() : Array[Array[ActorRef]] = {
         val rows = 10
         val cols = 20
-        //for (i <- 0 until rows) 
-        //    println(i)
-        val world = Array.ofDim[Creature](rows,cols)
-        //new Creature(1000, 0.8, "X")
+        val world = Array.ofDim[ActorRef](rows,cols)
+        val system = ActorSystem("CellularAutomataSystem")
         for ( i <- 0 until rows; j <- 0 until cols ) {
-            //val creature = new Creature(1.1, 2.2, "X")
-            val creature = new Creature()
-            world(i)(j) = creature
+            val nodeManager = system.actorOf(Props[NodeManager], name = "nodeManager_" + i + "_" + j)
+            world(i)(j) = nodeManager
         }
-        world(0)(0) = new Creature(1000, 2.2, "X")
-        //for ( i <- 0 until rows; j <- 0 until cols )
-        //    println(s"($i)($j) = ${world(i)(j)}")
         return world
     }
 
-    def printWorld(world : Array[Array[Creature]]) = {
+    def printWorld(world : Array[Array[ActorRef]]) = {
         val outerLength : Int = world.length
         val innerLength : Int = world(0).length
         for (i <- 0 until outerLength; j <- 0 until innerLength) {
-            print(world(i)(j).name + " ")
+            implicit val timeout = Timeout(5 seconds)
+            val nodeManager = world(i)(j)
+            val future = nodeManager ? "requestName"
+            val name = Await.result(future, timeout.duration).asInstanceOf[String]
+            print(name + " ")
             if (j == innerLength - 1) {
                 print("\n")
             }
