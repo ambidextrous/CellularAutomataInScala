611b62b9e4f814a05fd3c1daf2675281e89bfb70
Aidan <butleraidan@gmail.com>
2017-11-16 14:50:28 +0100

    Simulation running with species 1 and 2

--- cellularAutomata.scala
+++ cellularAutomata.scala
@@ -6,22 +6,23 @@ import scala.concurrent.{Await, ExecutionContext, Future}
 import scala.concurrent.duration._
 import scala.language.postfixOps
 import scala.collection.mutable.Map
+import scala.collection.mutable.ArrayBuffer
 
 class Creature(maxLifespanc: Int = 0, fitnessc: Double = 0, namec: String = "-", xc: Int, yc: Int) {
     val x: Int = xc
     val y: Int = yc
     val maxLifespan: Int = maxLifespanc
-    val deathTime: Int = maxLifespanc + getActualLifespan(maxLifespanc)
     val fitness: Double = fitnessc
     val name: String = namec
     val birthTime : Long = System.currentTimeMillis()
+    val deathTime: Long = birthTime + getActualLifespan(maxLifespanc)
 
     def getActualLifespan(upperLimit: Int): Int = {
         if (upperLimit < 1) {
             return 0
         }
         val r = scala.util.Random
-        val maxLifespan = r.nextInt(upperLimit)
+        val maxLifespan = r.nextInt(upperLimit) + 1
         return maxLifespan
     }
 
@@ -36,45 +37,52 @@ class Creature(maxLifespanc: Int = 0, fitnessc: Double = 0, namec: String = "-",
 
 class NodeManager(xc: Int, yc: Int) extends Actor {
     var resident: Creature = new Creature(0,0,"-",xc,yc)
+    var neighours: ArrayBuffer[ActorRef] = ArrayBuffer[ActorRef]()
 
     def receive = {
         case "requestName" =>
             sender ! resident.name
         case "checkStatus" =>
-            if (resident.maxLifespan != 0) {
-                val currentTime: Long = System.currentTimeMillis()
-                if (currentTime > resident.deathTime) {
-                    sender ! resident 
-                } else {
-                    val returnVal = new Creature(0,0,"-",resident.x,resident.y)
-                    sender ! returnVal
+            val currentTime: Long = System.currentTimeMillis()
+            if (resident.maxLifespan != 0 && currentTime > resident.deathTime) {
+                for (i <- 0 until neighours.length) {
+                    neighours(i) ! resident
                 }
-            } else {
-                val returnVal = new Creature(0,0,"-",resident.x,resident.y)
-                sender ! returnVal
-            }
+                resident = new Creature(0,0,"-",xc,yc)
+            } 
         case attacker: Creature => 
             val r = scala.util.Random
             if (r.nextFloat <= (attacker.fitness - resident.fitness)) {
                 resident = new Creature(attacker.maxLifespan, attacker.fitness, attacker.name, resident.x, resident.y)
             }
         case "speciesOne" => 
-            val speciesOneInstance = new Creature(1000,0.8,"1",resident.x,resident.y)
+            val speciesOneInstance = new Creature(10000,0.8,"1",resident.x,resident.y)
             resident = speciesOneInstance
+        case "speciesTwo" => 
+            val speciesTwoInstance = new Creature(5000,0.4,"2",resident.x,resident.y)
+            resident = speciesTwoInstance
+        case nbs: ArrayBuffer[ActorRef] => 
+            neighours = nbs
         case _ => println("Unknown message received")
     }
 }
 
 object cellularAutomata  {
     def main(args: Array[String]) {
-        val worldMap = generateWorld()
+        val rows: Int = 15
+        val cols: Int = 30
+        val worldMap = generateWorld(rows,cols)
         //runSimulation(world)
-        populateWorld(worldMap)
-        updateWorldState(worldMap,10,20)
+        populateWorld(worldMap,rows,cols)
+        allocateNeighbours(worldMap,rows,cols)
+        updateWorldState(worldMap,rows,cols)
     }
 
-    def populateWorld(worldMap: scala.collection.immutable.Map[String,ActorRef]) = {
-        worldMap("5_5") ! "speciesOne"
+    def populateWorld(worldMap: scala.collection.immutable.Map[String,ActorRef], rows: Int, cols: Int) = {
+        val bottomCorner = "0_0"
+        val topCorner = "" + (rows - 1) + "_" + (cols - 1)
+        worldMap(bottomCorner) ! "speciesOne"
+        worldMap(topCorner) ! "speciesTwo"
     }
 
     def runSimulation(worldMap: scala.collection.immutable.Map[String,ActorRef]) = {
@@ -84,26 +92,37 @@ object cellularAutomata  {
         }
     }
 
+    def allocateNeighbours(worldMap: scala.collection.immutable.Map[String,ActorRef], rows: Int, cols: Int) = {
+        for ( i <- 0 until rows; j <- 0 until cols) {
+            val creatureKey: String = "" + i + "_" + j
+            if (worldMap isDefinedAt creatureKey) {
+                val creature = worldMap(creatureKey)
+                val moves = List(-1, 0, 1)
+                var neighours: ArrayBuffer[ActorRef] = ArrayBuffer[ActorRef]()
+                for ( k <- 0 until moves.length; l <- 0 until moves.length) {
+                    val x: Int = i + moves(k)
+                    val y: Int = j + moves(l)
+                    val neighourKey: String = "" + x + "_" + y
+                    if (worldMap isDefinedAt neighourKey) {
+                        if ( x != i || y != j) {
+                            neighours += worldMap(neighourKey)
+                        }
+                    }
+                }
+                creature ! neighours
+            } 
+        }
+    }
+
     def updateWorldState(worldMap: scala.collection.immutable.Map[String,ActorRef], rows: Int, cols: Int) = {
-        val printFrequency: Int = 2000
+        val printFrequency: Int = 500
         var startTime: Long = System.currentTimeMillis() - printFrequency
         while (true) {
             for ( i <- 0 until rows; j <- 0 until cols ) {
                 implicit val timeout = Timeout(5 seconds)
                 val key = "" + i + "_" + j
                 val nodeManager = worldMap(key)
-                val future = nodeManager ? "checkStatus"
-                val creature = Await.result(future, timeout.duration).asInstanceOf[Creature]
-                if (creature.maxLifespan != 0) {
-                    val moves = List(-1, 0, 1)
-                    for ( k <- 0 until moves.length; l <- 0 until moves.length) {
-                        val neighbourKey: String = "" + (creature.x + i) + "_" + (creature.y + j)
-                        if (worldMap isDefinedAt neighbourKey) {
-                            worldMap(neighbourKey) ! creature
-                        }
-                        
-                    }
-                }
+                nodeManager ! "checkStatus"
             }
             val currentTime: Long = System.currentTimeMillis()
             if (currentTime > startTime + printFrequency) {
@@ -124,10 +143,8 @@ object cellularAutomata  {
         }
     }
 
-    def generateWorld() : scala.collection.immutable.Map[String,ActorRef] = {
+    def generateWorld(rows: Int, cols: Int) : scala.collection.immutable.Map[String,ActorRef] = {
     //def generateWorld() : Array[Array[ActorRef]] = {
-        val rows = 10
-        val cols = 20
         val world = Array.ofDim[ActorRef](rows,cols)
         val system = ActorSystem("CellularAutomataSystem")
         var nodeManagerMap: Map[String,ActorRef] =  Map()
